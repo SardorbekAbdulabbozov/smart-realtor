1cfb55b6b764b8fe39c1b39b130268124a6fd60c
SardorbekAbdulabbozov <sabdulabbosov@gmail.com>
2022-11-13 20:13:12 +0500

    should work

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,21 @@
-import 'package:smart_realtor_server/server.dart';
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
 /// This is the starting point for your Serverpod server. Typically, there is
 /// no need to modify this file.
-void main(List<String> args) {
-  run(args);
+void main() async {
+  var handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+  var server = await shelf_io.serve(handler, 'localhost', 8080);
+// Enable content compression
+  server.autoCompress = true;
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+Future<Response> _echoRequest(Request request) async {
+  final result =
+      await File('${Directory.current.path}/public/index.html').readAsString();
+  return Response.ok(result, headers: {'content-type': 'text/html'});
 }
